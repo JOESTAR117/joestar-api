1a59c7906a6acbbd35479b9f12ec2f677d6901c1
Joestar117 <juniorrios1231@gmail.com>
2023-03-12 21:18:54 -0300

    feat: migrate and database sqlite created

--- db/migrate/20230313000603_create_jojos.rb
+++ db/migrate/20230313000603_create_jojos.rb
@@ -1,7 +1,7 @@
 class CreateJojos < ActiveRecord::Migration[6.1]
   def change
     create_table :jojos do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
