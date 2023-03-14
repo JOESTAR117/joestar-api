cf70acbed96c08eafb7e5a9dd13db32d0944afbc
Joestar117 <juniorrios1231@gmail.com>
2023-03-14 15:44:07 -0300

    feat: jojo name is unique created

--- app/models/jojo.rb
+++ app/models/jojo.rb
@@ -1,10 +1,10 @@
 class Jojo < ApplicationRecord
   validates :name, :token, presence: true
+  validates :name, uniqueness: { scope: :token, case_sensitive: false }
 
   scope :by_token, ->(token) { where(token:) }
 
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
 
-  # Ex:- scope :active, -> {where(:active => true)}
   scope :sorted_by_name, -> { order(:name) }
 end
